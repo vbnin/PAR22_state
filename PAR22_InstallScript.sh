524fc6dea8051256413ffa86fc91b36513c3fdb8
Vincent <31508339+vbnin@users.noreply.github.com>
2017-09-04 18:11:03 +0200

    Update PAR22_InstallScript.sh

--- PAR22_InstallScript.sh
+++ PAR22_InstallScript.sh
@@ -3,3 +3,4 @@
 cd ..
 sudo cp PAR22_state/ /usr/local/bin/
 echo -e "$(sudo crontab -u root -l)\n@reboot python /usr/local/bin/PAR22_state/PAR22_state/core.py -c "config.ini" > /var/log/PAR22_state.log 2>&1" | sudo crontab -u root -
+echo "*** Installation terminée ***"
