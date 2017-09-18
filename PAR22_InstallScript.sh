8030a426c1d899962959363dcf194b80642fd401
vbnin <vincent.bonnin@outlook.com>
2017-09-18 17:45:39 +0200

    modif script de lancement + Libraries

--- PAR22_InstallScript.sh
+++ PAR22_InstallScript.sh
@@ -7,6 +7,8 @@ cd /usr/local/bin/PAR22_state/PAR22_state/
 sudo chmod +x core.py
 sudo chmod +x Libraries.py
 echo "*** Ajout des droits d'exécution ***"
-echo -e "$(crontab -u pi -l)\n@reboot sudo /usr/bin/python /usr/local/bin/PAR22_state/PAR22_state/core.py -c '/usr/local/bin/PAR22_state/PAR22_state/config.ini'" | crontab -u pi -l
+echo -e "$(crontab -u pi -l)\n@reboot sudo /usr/bin/python /usr/local/bin/PAR22_state/PAR22_state/core.py -c '/usr/local/bin/PAR22_state/PAR22_state/config.ini'" | crontab -u pi -
 echo "*** Activation du script au reboot via sudo crontab ***"
-echo "*** Installation terminée ***"
+echo "*** Installation terminée, reboot dans 15 secondes. Pour annuler, pressez 'Ctrl + C' ***"
+sleep 15
+sudo reboot
