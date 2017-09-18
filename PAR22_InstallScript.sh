c761c1387bdfa1c5d0de079b94d40a9ceeb0dade
vbnin <vincent.bonnin@outlook.com>
2017-09-18 16:58:28 +0200

    test

--- PAR22_InstallScript.sh
+++ PAR22_InstallScript.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cd /home/pi/
-sudo cp -fR PAR22_state/ /usr/local/bin/
+sudo mv -f PAR22_state/ /usr/local/bin/
+echo "*** Déplacement du dossier dans /usr/local/bin ***"
 cd /usr/local/bin/PAR22_state/PAR22_state/
 sudo chmod +x core.py
-echo -e "$(sudo crontab -u root -l)\n@reboot python /usr/local/bin/PAR22_state/PAR22_state/core.py -c 'config.ini'" | sudo crontab -u root -
+sudo chmod +x Libraries.py
+echo "*** Ajout des droits d'exécution ***"
+echo -e "$(crontab -u pi -l)\n@reboot sudo /usr/bin/python3 /usr/local/bin/PAR22_state/PAR22_state/core.py -c '/usr/local/bin/PAR22_state/PAR22_state/config.ini'" | crontab -u pi -l
+echo "*** Activation du script au reboot via sudo crontab ***"
 echo "*** Installation terminée ***"
